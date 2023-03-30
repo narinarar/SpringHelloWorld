f4528bb082d159d4d0f5d6b0f7d445722725da32
Narin Arar <narinarar1@gmail.com>
2023-03-30 20:13:39 +0300

    RequestMapping

--- src/main/java/com/java/controller/HelloController.java
+++ src/main/java/com/java/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.java.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
 
-    // localHost:8080/hello
+    // localHost:8080/api/hello
     @GetMapping(path = "/hello")
     public String sayHello(){
         return "Hello World!!";
